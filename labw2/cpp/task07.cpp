24b2bde67e7c34d20ed281771404dd493a054b3f
GeorgeReaganG <danishalizx4@gmail.com>
2022-12-21 00:05:10 +0500

    PD work

---
+++ labw2/cpp/task07.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+main(){
+system("color 24");
+cout << "       .::----::.." <<endl;
+cout << "   .-----------------." <<endl;
+cout << "  .-----------------." <<endl;
+cout << " -----------------:." <<endl;
+cout << ":--------------::." <<endl;
+cout << "--------------:." <<endl;
+cout << ":--------------:." <<endl;
+cout << " :--------------:." <<endl;
+cout << "   .--------------:" <<endl;
+cout << "     .:-------------:." <<endl;
+cout << "         -::----::-" <<endl;
+
+
+
+
+
+}
+
+
+                        			
+
+
+
+
---
+++ labw2/cpp/task9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+main(){
+cout << "--------------------------------------" << endl;
+cout << "       O        ^__^" << endl;
+cout << "         O      (oo )\\____________" << endl;
+cout << "                (___) \\            ) \\/\\     " << endl;
+cout << "                     | |--------w |" << endl;
+cout << "                     | |        | |" << endl;
+
+
+
+
+
+
+
+
+}
